9e585d2b2df0c754e4de31192892b68903a4113d
Denerson Nobre <dnolive@gmail.com>
2016-12-18 11:56:33 -0200

    capitulo3-aula4-v2

--- app/app.ts
+++ app/app.ts
@@ -5,18 +5,38 @@ import {HomePage} from './pages/home/home';
 
 
 @Component({
-  template: '<ion-nav [root]="rootPage"></ion-nav>'
+  templateUrl: 'build/app.html'
 })
+
 export class MyApp {
-  rootPage: any = HomePage;
+
+  pages: Array<{
+    component: any,
+    title: string,
+    icon: string
+  }>;
+
+  home: any     = HomePage;
+  rootPage: any = this.home;
 
   constructor(platform: Platform) {
+    this.pages = [
+      {component: HomePage,
+      title: 'Início',
+      icon: 'home'}
+    ];
+
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
-      // Here you can do any higher level native things you might need.
+      // Here you cmean do any higher level native things you might need.
       StatusBar.styleDefault();
     });
   }
+
+  openPage(page): void {
+    this.rootPage = page;
+  }
+
 }
 
 ionicBootstrap(MyApp);
